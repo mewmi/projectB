99800a03eaa76befed19def42de558596c6baaf7
joao <njoao99@gmail.com>
2023-01-14 11:38:42 +0000

    added image

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import echosound from "../images/echosound.png";
 
 const HomePage = () => {
   return (
     <div>
       Upload songs and their respective links and create funny images from their
       titles
+      <img src={echosound} alt="echosound" />
     </div>
   );
 };
