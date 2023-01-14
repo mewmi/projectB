84099a46c8a63e77620861499c81a74857be6ecd
Sumia EOs <seosultra@gmail.com>
2023-01-14 16:54:31 +0100

    second week

---
+++ client/src/pages/AccountPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AccountPage = () => {
+  return (
+    <div>
+      AccountPage
+      <input value="name">Change Name</input>
+      <input value="password">Change password</input>
+      <button>Submit</button>
+    </div>
+  );
+};
+
+export default AccountPage;
