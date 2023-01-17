dd7d9a938da753b44d1329544642520a8708d3fd
Julia Lipniewicz <julialipniewicz@yahoo.de>
2023-01-17 13:11:05 +0100

    tailwind

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,40 +12,38 @@ const Navbar = () => {
 
   return (
     <div className="w-full dark:bg-black bg-gray-400 flex items-center justify-center">
-      <ul className="flex flex-col p-4 md:flex-row">
-        <li>
-          <img
-            className="mx-auto space-x-4 items-center"
-            src={EchoSmall}
-            alt="echosound"
-          />
-        </li>
+      <ul className="flex flex-col p-2 md:flex-row">
         {user && (
           <>
             <li>
+              <img className=" items-center" src={EchoSmall} alt="echosound" />
+            </li>
+            <li>
               <Link to="/">
-                <button className="p-3 rounded-xl bg-white shadow-lg space-x-10 mx-auto text-center sm:text-left">
+                <button className="p-2 rounded-xl bg-white shadow-lg space-x-10 mx-auto text-center sm:text-left">
                   HomePage
                 </button>
               </Link>
             </li>
+
             <li>
               <Link to="/songs">
-                <button className="p-3 rounded-xl bg-white shadow-lg">
+                <button className="p-2 rounded-xl bg-white shadow-lg">
                   All Songs
                 </button>
               </Link>
             </li>
             <li>
               <Link to="/new-song">
-                <button className="p-3 rounded-xl bg-white shadow-lg">
+                <button className="p-2 rounded-xl bg-white shadow-lg">
                   Add new Song
                 </button>
               </Link>
             </li>
+
             <li>
               <Link to="/account">
-                <button className="p-3 rounded-xl bg-white shadow-lg">
+                <button className="p-2 rounded-xl bg-white shadow-lg">
                   Your account
                 </button>
               </Link>
@@ -53,7 +51,7 @@ const Navbar = () => {
             <li>
               <button
                 onClick={handleLogout}
-                className="p-3 rounded-xl bg-white shadow-lg"
+                className="p-2 rounded-xl bg-white shadow-lg"
               >
                 Logout
               </button>
@@ -61,11 +59,7 @@ const Navbar = () => {
           </>
         )}
 
-        {(user && (
-          <li>
-            <span>{user.name}</span>
-          </li>
-        )) || (
+        {!user && (
           <>
             <li>
               <Link to="/login">
--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -5,9 +5,9 @@ const AccountPage = () => {
   const { user } = useAuthContext();
   return (
     <div>
-      <div>Welcome {user.name}</div>
+      <div className="text-center">Welcome {user.name}</div>
       <br />
-      <div>
+      <div className="text-center">
         We are happy you decided to be part of EchoSound. EchoSound is there to
         help you access your favorite music videos at any time of the day.
       </div>
