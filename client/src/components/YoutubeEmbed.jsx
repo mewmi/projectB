b0a1e4a94bd067b8aaf5fa34abcfdb51006b96e5
joao <njoao99@gmail.com>
2023-01-21 09:30:25 +0000

    fixed video

--- client/src/components/YoutubeEmbed.jsx
+++ client/src/components/YoutubeEmbed.jsx
@@ -3,16 +3,13 @@ import React from "react";
 const YoutubeEmbed = ({ embedId }) => (
   <div className="videoResponsive">
     <iframe
-      width="853"
-      height="480"
+      width="500"
+      height="281"
       src={`https://www.youtube.com/embed/${embedId}`}
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
       title="Embedded youtube"
       style={{
-        width: "auto",
-        /*  minWidth: "500px" */ /* , */ height: "auto",
-        display: "flex",
         justifyContent: "center",
         marginTop: "20px",
       }}
